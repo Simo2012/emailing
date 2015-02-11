a0d75f18221b0b579ece51cb880720806a6c725d
Hrshikesh MEJARI <Mejari@natexo.com>
2015-02-11 16:57:51 +0100

    Shift slider code from Global to Index

--- src/Web/WebBundle/Resources/public/js/global/Global.js
+++ src/Web/WebBundle/Resources/public/js/global/Global.js
@@ -27,39 +27,7 @@ Global.prototype = {
             $('#div_RBZ_menuMobile').toggleClass('RBZ_hide');
             return false;
         });
-        // ==== RUB Slider ====
-        this.rubSlider();
     }, // ready
-
-    /**
-     * Head slider
-     */
-    rubSlider: function()
-    {
-        $(document).ready(function() {
-          $("#div_RUB_sliderContain").owlCarousel({
-//            items: 1,
-//            autoPlay: 3000,
-//            navigation: true,
-//            navigationText: ["<", ">"],
-//            itemsTablet: [767,1],
-//            itemsTabletSmall: [767,1],
-//            itemsMobile: [480,1],
-//            navigation : true,
-//            singleItem : true,
-//            transitionStyle : "fade"
-              loop:true,
-              margin:10,
-              nav:true,
-              items: 1,
-              navText: false,
-              dots: true,
-              animateOut: 'fadeOut',
-              animatein: 'fadein',
-              // autoplay: true
-          });
-        });
-    }, // rubSlider
 
     /**
      * Token de fin
