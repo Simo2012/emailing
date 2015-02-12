1505910e26e96cdd493d994ecf411779ad520dd9
Julien <lepine@natexo.com>
2015-02-12 20:11:55 +0100

    Fin de l'intégration des popups de login et register

--- src/Web/WebBundle/Resources/public/js/default/Default.js
+++ src/Web/WebBundle/Resources/public/js/default/Default.js
@@ -16,21 +16,65 @@ Default.prototype = {
     /**
      * Traitements lancés en fin de chargement de la page
      */
-    ready : function()
+    ready: function()
     {
-        // Ouverture des popup de login
+        // Ouverture du popup de login
+        Default.prototype._manageLoginPopupOpening();
+        // Fermeture du popup de login
+        Default.prototype._manageLoginPopupClosing();
+        // Ouverture du popup de register
+        Default.prototype._manageRegisterPopupOpening();
+        // Fermeture du popup de register
+        Default.prototype._manageRegisterPopupClosing();
+        // Ouverture du popup de login depuis le popup de register
+        $(document).on("click", "#div_RBZ_registerPopup #div_RBZ_notice a.RBZ_login", function() {
+            $("#div_RBZ_registerPopup #div_RBZ_close").click();
+            $("span.RBZ_default_login a").click();
+            return false;
+        });
+        // Ouverture du popup de login depuis le popup de register
+        $(document).on("click", "#div_RBZ_loginPopup #div_RBZ_notice a.RBZ_register", function() {
+            $("#div_RBZ_loginPopup #div_RBZ_close").click();
+            $("span.RBZ_default_register a").click();
+            return false;
+        });
+    }, // ready
+
+    _manageLoginPopupOpening: function()
+    {
         $(document).on("click", "span.RBZ_default_login a", function() {
             var lsUrl = $(this).attr('hrefbis');
             Default.prototype.ajaxPopupLogin(lsUrl);
             return false;
         });
-        // Ouverture des popup de register
+    }, // _manageLoginPopupOpening
+
+    _manageLoginPopupClosing: function()
+    {
+        $(document).on("click", "#div_RBZ_loginPopup #div_RBZ_close", function() {
+            $("#div_RBZ_loginPopup").detach();
+            $("#div_RBZ_loginShadow").hide();
+            return false;
+        });
+    }, // _manageLoginPopupClosing
+
+    _manageRegisterPopupOpening: function()
+    {
         $(document).on("click", "span.RBZ_default_register a", function() {
             var lsUrl = $(this).attr('hrefbis');
             Default.prototype.ajaxPopupRegister(lsUrl);
             return false;
         });
-    }, // ready
+    }, // _manageRegisterPopupOpening
+
+    _manageRegisterPopupClosing: function()
+    {
+        $(document).on("click", "#div_RBZ_registerPopup #div_RBZ_close", function() {
+            $("#div_RBZ_registerPopup").detach();
+            $("#div_RBZ_registerShadow").hide();
+            return false;
+        });
+    }, // _manageRegisterPopupClosing
 
     /**
      * Appel d'une popup en ajax
