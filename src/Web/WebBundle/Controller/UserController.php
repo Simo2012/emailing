8cfc7211a2b6f16b650b90bfcbb9d79733370cc3
GHAZI <GHAZI@192.168.43.85>
2015-02-13 09:53:18 +0100

    By Mohammed : Etapes Ajout RIb

--- src/Web/WebBundle/Controller/UserController.php
+++ src/Web/WebBundle/Controller/UserController.php
@@ -1,6 +1,8 @@
 <?php
+
 namespace Web\WebBundle\Controller;
 
+use Web\WebBundle\Form\UserTypeBIC;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,55 +18,85 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
  * @version 1.0
  * @package Rubizz
  */
-class UserController extends Controller
-{
+class UserController extends Controller {
+
     /**
      * Page d'accueil
      *
      * @Template()
      */
-    public function profileAction()
-    {
+    public function profileAction() {
         return array();
-    } // profileAction
+    }
+
+// profileAction
 
     /**
      * Page particular
      *
      * @Template()
      */
-    public function particularsAction()
-    {
+    public function particularsAction() {
         return array();
-    } // particularAction
+    }
+
+// particularAction
 
     /**
      * Page rib
      *
      * @Template()
      */
-    public function ribAction()
-    {
-        return array();
-    } // ribAction
+    public function ribAction($piUserId) {
+        $loManager = $this->getDoctrine()->getManager();
+
+        $loUser = $loManager->getRepository('WebWebBundle:User')->find($piUserId);
+
+        $loForm = $this->createForm(
+                new UserTypeBIC($this->get('natexo_tool.filter.encrypt'),
+                        $this->get('natexo_tool.filter.decrypt')), $loUser
+        );
+        // ==== Traitement de la saisie ====
+        $loRequest = $this->getRequest();
+        if ($loRequest->isMethod('POST')) {
+            $loForm->bind($loRequest);
+            try {
+                $loUser->setDateUpdate(new \DateTime('now'));
+                $loManager->flush();
+                var_dump('ok');
+                exit();
+            } catch (DBALException $poException) {
+                var_dump($poException);
+            }
+        }
+
+        return array(
+            'form' => $loForm->createView(),
+            'UserId' => $loUser->getId()
+        );
+    }
+
+// ribAction
 
     /**
      * Page cagnotte
      *
      * @Template()
      */
-    public function potAction()
-    {
+    public function potAction() {
         return array();
-    } // potAction
+    }
+
+// potAction
 
     /**
      * Page encaisser la cagnotte
      *
      * @Template()
      */
-    public function cashInAction()
-    {
+    public function cashInAction() {
         return array();
-    } // cashInAction
+    }
+
+// cashInAction
 }
