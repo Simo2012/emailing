25d45eb3dc0530e13959f919bb1caeb37467c571
Philippe <laurent@natexo.com>
2015-02-16 19:58:34 +0100

    Ajout de Recommendation

---
+++ etc/sql/RUBIZZ/Recommendation.sql
@@ -0,0 +1,33 @@
+-- ===========================
+-- Table des recommandations
+-- ===========================
+CREATE TABLE IF NOT EXISTS RUBIZZ.recommendation (
+    id           int(10) unsigned     NOT NULL AUTO_INCREMENT COMMENT 'Unique id',
+    user_id      int(10) unsigned     NOT NULL COMMENT 'Corresponding user',
+    offer_id     int(10) unsigned     NOT NULL COMMENT 'Corresponding offer',
+    type         enum('email', 'facebook', 'twitter') NOT NULL COMMENT 'Type of recommendation',
+    date_create  datetime             NOT NULL COMMENT 'Creation date',
+
+    PRIMARY KEY (id),
+    KEY recommendationUser_idx (user_id),
+    KEY recommendationOffer_idx (offer_id),
+    CONSTRAINT recommendationUser_fk FOREIGN KEY (user_id) REFERENCES RUBIZZ.user (id) ON DELETE RESTRICT,
+    CONSTRAINT recommendationOffer_fk FOREIGN KEY (offer_id) REFERENCES RUBIZZ.offer (id) ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Recommendations';
+
+-- ==== droits ====
+GRANT select, insert, update, delete ON RUBIZZ.recommendation to web@'%';
+
+-- ===========================================================
+-- Table de liaison (n,m) entre "recommendation" et "contact"
+-- ===========================================================
+CREATE TABLE IF NOT EXISTS RUBIZZ.recommendation_contacts (
+    recommendation_id int(10) unsigned     NOT NULL COMMENT 'Recommendation id',
+    contact_id        int(10) unsigned     NOT NULL COMMENT 'Contact id',
+    PRIMARY KEY (recommendation_id, contact_id),
+    CONSTRAINT recommendationContactRecommendation_fk FOREIGN KEY (recommendation_id) REFERENCES RUBIZZ.recommendation (id) ON DELETE CASCADE,
+    CONSTRAINT recommendationContactContact_fk FOREIGN KEY (contact_id) REFERENCES RUBIZZ.contact (id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Recommendations linked with contacts';
+
+-- ==== droits ====
+GRANT select, insert, update, delete ON RUBIZZ.recommendation_contacts to web@'%';
---
+++ src/Web/WebBundle/Entity/Recommendation.php
@@ -0,0 +1,216 @@
+<?php
+
+namespace Web\WebBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Recommendation
+ *
+ * @ORM\Table(name="recommendation", indexes={@ORM\Index(name="recommendationUser_idx", columns={"user_id"}), @ORM\Index(name="recommendationOffer_idx", columns={"offer_id"})})
+ * @ORM\Entity
+ */
+class Recommendation
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", nullable=false)
+     */
+    private $type;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_create", type="datetime", nullable=false)
+     */
+    private $dateCreate;
+
+    /**
+     * @var \User
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     * })
+     */
+    private $user;
+
+    /**
+     * @var \Offer
+     *
+     * @ORM\ManyToOne(targetEntity="Offer")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="offer_id", referencedColumnName="id")
+     * })
+     */
+    private $offer;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     *
+     * @ORM\ManyToMany(targetEntity="Contact", inversedBy="recommendation")
+     * @ORM\JoinTable(name="recommendation_contacts",
+     *   joinColumns={
+     *     @ORM\JoinColumn(name="recommendation_id", referencedColumnName="id")
+     *   },
+     *   inverseJoinColumns={
+     *     @ORM\JoinColumn(name="contact_id", referencedColumnName="id")
+     *   }
+     * )
+     */
+    private $contact;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->contact = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Recommendation
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set dateCreate
+     *
+     * @param \DateTime $dateCreate
+     * @return Recommendation
+     */
+    public function setDateCreate($dateCreate)
+    {
+        $this->dateCreate = $dateCreate;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreate
+     *
+     * @return \DateTime 
+     */
+    public function getDateCreate()
+    {
+        return $this->dateCreate;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Web\WebBundle\Entity\User $user
+     * @return Recommendation
+     */
+    public function setUser(\Web\WebBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Web\WebBundle\Entity\User 
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set offer
+     *
+     * @param \Web\WebBundle\Entity\Offer $offer
+     * @return Recommendation
+     */
+    public function setOffer(\Web\WebBundle\Entity\Offer $offer = null)
+    {
+        $this->offer = $offer;
+
+        return $this;
+    }
+
+    /**
+     * Get offer
+     *
+     * @return \Web\WebBundle\Entity\Offer 
+     */
+    public function getOffer()
+    {
+        return $this->offer;
+    }
+
+    /**
+     * Add contact
+     *
+     * @param \Web\WebBundle\Entity\Contact $contact
+     * @return Recommendation
+     */
+    public function addContact(\Web\WebBundle\Entity\Contact $contact)
+    {
+        $this->contact[] = $contact;
+
+        return $this;
+    }
+
+    /**
+     * Remove contact
+     *
+     * @param \Web\WebBundle\Entity\Contact $contact
+     */
+    public function removeContact(\Web\WebBundle\Entity\Contact $contact)
+    {
+        $this->contact->removeElement($contact);
+    }
+
+    /**
+     * Get contact
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getContact()
+    {
+        return $this->contact;
+    }
+}
