4a5df99a7dea974a3b4d397f79e054b91435ca98
Philippe <laurent@natexo.com>
2015-02-16 14:22:15 +0100

    Modification de l'entité Offer

--- etc/sql/RUBIZZ/Offer.sql
+++ etc/sql/RUBIZZ/Offer.sql
@@ -2,21 +2,33 @@
 -- Table des offres de Rubizz
 -- ===========================
 CREATE TABLE IF NOT EXISTS RUBIZZ.offer (
-    id            int(10) unsigned    NOT NULL AUTO_INCREMENT COMMENT 'Offer unique id',
-    country       char(2)             NOT NULL COMMENT 'Country code',
-    date_create   datetime            NOT NULL COMMENT 'Creation date',
-    date_update   datetime            NOT NULL COMMENT 'Update date',
-    title         varchar(200)        NOT NULL COMMENT 'Offer title',
-    subtitle      varchar(200)        NOT NULL COMMENT 'Offer subtitle',
-    text          varchar(4096)       NOT NULL COMMENT 'Offer text',
-    url           varchar(200)        NOT NULL COMMENT 'Offer URL (orginal one)',
-    compensation  decimal(10,2)       NOT NULL COMMENT 'Compensation for thet offer',
-    active        tinyint(3) unsigned NOT NULL COMMENT 'User active or not',
-    platform      enum('tradedoubler', 'cake') NOT NULL COMMENT 'Name of the platform (website)',
-    platform_id   varchar(200)        NOT NULL COMMENT 'Platform unique identifier',
-    category      set('game', 'estimate', 'e-commerce', 'travel', 'finance', 'untertainment', 'special-offer') NOT NULL COMMENT 'List of possible tags',
+    id                int(10) unsigned     NOT NULL AUTO_INCREMENT COMMENT 'Offer unique id',
+    country           char(2)              NOT NULL COMMENT 'Country code',
+    date_create       datetime             NOT NULL COMMENT 'Creation date',
+    date_update       datetime             NOT NULL COMMENT 'Update date',
+    title             varchar(200)         NOT NULL COMMENT 'Offer title',
+    text              varchar(4096)        NOT NULL COMMENT 'Offer text',
+    url               varchar(200)         NOT NULL COMMENT 'Offer URL (orginal one)',
+    compensation      decimal(10,2)        NOT NULL COMMENT 'Compensation for thet offer',
+    active            tinyint(3) unsigned  NOT NULL COMMENT 'User active or not',
+    date_start        datetime             NOT NULL COMMENT 'Start of the offer availability',
+    date_end          datetime             NOT NULL COMMENT 'End of the offer availability',
+    advertiser_cookie smallint(5) unsigned NOT NULL COMMENT 'Number of days for the cookie',
+    member_cookie     smallint(5) unsigned NOT NULL COMMENT 'Number of days for the cookie',
+    platform          enum('manual', 'tradedoubler', 'cake') NOT NULL COMMENT 'Name of the platform (website)',
+    platform_id       varchar(200)             NULL COMMENT 'Platform unique identifier',
+    category          set('game', 'estimate', 'e-commerce', 'travel', 'finance', 'untertainment', 'special-offer') NOT NULL COMMENT 'List of possible tags',
+    publishing        set('email', 'facebook', 'twitter')    NOT NULL COMMENT 'List of possible publishing',
+    subsidiary_id     int(10) unsigned     NOT NULL COMMENT 'Corresponding subsidiary',
+    rem_type          enum('CPC', 'CPL', 'CPA%', 'CPA')      NOT NULL COMMENT 'Remuneration type',
+    rem_advertiser    decimal(10,2)        NOT NULL COMMENT 'Remuneration amount or % from the advertiser',
+    rem_volume        int(10) unsigned     NOT NULL COMMENT 'Number of available offers',
+    rem_illimited     tinyint(3) unsigned  NOT NULL COMMENT 'If the volume is illimited',
+    rem_member        decimal(10,2)        NOT NULL COMMENT 'Remuneration amount or % for the member',
+
     PRIMARY KEY (id),
-    KEY offerCountryActive_idx (country, active, category)
+    KEY offerCountryActive_idx (country, active, category),
+    CONSTRAINT offerSubsidiary_fk FOREIGN KEY (subsidiary_id) REFERENCES SYNCHRO.subsidiary (id) ON DELETE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Offers';
 
 -- ==== droits ====
--- src/Web/WebBundle/Entity/Offer.php
+++ src/Web/WebBundle/Entity/Offer.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * Offer
  *
- * @ORM\Table(name="offer", indexes={@ORM\Index(name="offerCountryActive_idx", columns={"country", "active", "category"})})
+ * @ORM\Table(name="offer", indexes={@ORM\Index(name="offerCountryActive_idx", columns={"country", "active", "category"}), @ORM\Index(name="offerSubsidiary_fk", columns={"subsidiary_id"})})
  * @ORM\Entity
  */
 class Offer
@@ -52,13 +52,6 @@ class Offer
     /**
      * @var string
      *
-     * @ORM\Column(name="subtitle", type="string", length=200, nullable=false)
-     */
-    private $subtitle;
-
-    /**
-     * @var string
-     *
      * @ORM\Column(name="text", type="string", length=4096, nullable=false)
      */
     private $text;
@@ -85,6 +78,34 @@ class Offer
     private $active;
 
     /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_start", type="datetime", nullable=false)
+     */
+    private $dateStart;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_end", type="datetime", nullable=false)
+     */
+    private $dateEnd;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="advertiser_cookie", type="smallint", nullable=false)
+     */
+    private $advertiserCookie;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="member_cookie", type="smallint", nullable=false)
+     */
+    private $memberCookie;
+
+    /**
      * @var string
      *
      * @ORM\Column(name="platform", type="string", nullable=false)
@@ -94,16 +115,68 @@ class Offer
     /**
      * @var string
      *
-     * @ORM\Column(name="platform_id", type="string", length=200, nullable=false)
+     * @ORM\Column(name="platform_id", type="string", length=200, nullable=true)
      */
     private $platformId;
 
     /**
      * @var simplearray
      *
-     * @ORM\Column(name="category", type="simple_array", nullable=false)
+     * @ORM\Column(name="category", type="simplearray", nullable=false)
      */
     private $category;
+
+    /**
+     * @var simplearray
+     *
+     * @ORM\Column(name="publishing", type="simplearray", nullable=false)
+     */
+    private $publishing;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="rem_type", type="string", nullable=false)
+     */
+    private $remType;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="rem_advertiser", type="decimal", precision=10, scale=2, nullable=false)
+     */
+    private $remAdvertiser;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="rem_volume", type="integer", nullable=false)
+     */
+    private $remVolume;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="rem_illimited", type="boolean", nullable=false)
+     */
+    private $remIllimited;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="rem_member", type="decimal", precision=10, scale=2, nullable=false)
+     */
+    private $remMember;
+
+    /**
+     * @var \Subsidiary
+     *
+     * @ORM\ManyToOne(targetEntity="Web\WebBundle\Entity\Synchro\Subsidiary")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="subsidiary_id", referencedColumnName="id")
+     * })
+     */
+    private $subsidiary;
 
 
 
@@ -210,29 +283,6 @@ class Offer
     }
 
     /**
-     * Set subtitle
-     *
-     * @param string $subtitle
-     * @return Offer
-     */
-    public function setSubtitle($subtitle)
-    {
-        $this->subtitle = $subtitle;
-
-        return $this;
-    }
-
-    /**
-     * Get subtitle
-     *
-     * @return string
-     */
-    public function getSubtitle()
-    {
-        return $this->subtitle;
-    }
-
-    /**
      * Set text
      *
      * @param string $text
@@ -325,6 +375,98 @@ class Offer
     }
 
     /**
+     * Set dateStart
+     *
+     * @param \DateTime $dateStart
+     * @return Offer
+     */
+    public function setDateStart($dateStart)
+    {
+        $this->dateStart = $dateStart;
+
+        return $this;
+    }
+
+    /**
+     * Get dateStart
+     *
+     * @return \DateTime
+     */
+    public function getDateStart()
+    {
+        return $this->dateStart;
+    }
+
+    /**
+     * Set dateEnd
+     *
+     * @param \DateTime $dateEnd
+     * @return Offer
+     */
+    public function setDateEnd($dateEnd)
+    {
+        $this->dateEnd = $dateEnd;
+
+        return $this;
+    }
+
+    /**
+     * Get dateEnd
+     *
+     * @return \DateTime
+     */
+    public function getDateEnd()
+    {
+        return $this->dateEnd;
+    }
+
+    /**
+     * Set advertiserCookie
+     *
+     * @param integer $advertiserCookie
+     * @return Offer
+     */
+    public function setAdvertiserCookie($advertiserCookie)
+    {
+        $this->advertiserCookie = $advertiserCookie;
+
+        return $this;
+    }
+
+    /**
+     * Get advertiserCookie
+     *
+     * @return integer
+     */
+    public function getAdvertiserCookie()
+    {
+        return $this->advertiserCookie;
+    }
+
+    /**
+     * Set memberCookie
+     *
+     * @param integer $memberCookie
+     * @return Offer
+     */
+    public function setMemberCookie($memberCookie)
+    {
+        $this->memberCookie = $memberCookie;
+
+        return $this;
+    }
+
+    /**
+     * Get memberCookie
+     *
+     * @return integer
+     */
+    public function getMemberCookie()
+    {
+        return $this->memberCookie;
+    }
+
+    /**
      * Set platform
      *
      * @param string $platform
@@ -373,10 +515,10 @@ class Offer
     /**
      * Set category
      *
-     * @param array $category
+     * @param \simplearray $category
      * @return Offer
      */
-    public function setCategory(array $category)
+    public function setCategory(\simplearray $category)
     {
         $this->category = $category;
 
@@ -391,5 +533,166 @@ class Offer
     public function getCategory()
     {
         return $this->category;
+    }
+
+    /**
+     * Set publishing
+     *
+     * @param \simplearray $publishing
+     * @return Offer
+     */
+    public function setPublishing(\simplearray $publishing)
+    {
+        $this->publishing = $publishing;
+
+        return $this;
+    }
+
+    /**
+     * Get publishing
+     *
+     * @return \simplearray
+     */
+    public function getPublishing()
+    {
+        return $this->publishing;
+    }
+
+    /**
+     * Set remType
+     *
+     * @param string $remType
+     * @return Offer
+     */
+    public function setRemType($remType)
+    {
+        $this->remType = $remType;
+
+        return $this;
+    }
+
+    /**
+     * Get remType
+     *
+     * @return string
+     */
+    public function getRemType()
+    {
+        return $this->remType;
+    }
+
+    /**
+     * Set remAdvertiser
+     *
+     * @param string $remAdvertiser
+     * @return Offer
+     */
+    public function setRemAdvertiser($remAdvertiser)
+    {
+        $this->remAdvertiser = $remAdvertiser;
+
+        return $this;
+    }
+
+    /**
+     * Get remAdvertiser
+     *
+     * @return string
+     */
+    public function getRemAdvertiser()
+    {
+        return $this->remAdvertiser;
+    }
+
+    /**
+     * Set remVolume
+     *
+     * @param integer $remVolume
+     * @return Offer
+     */
+    public function setRemVolume($remVolume)
+    {
+        $this->remVolume = $remVolume;
+
+        return $this;
+    }
+
+    /**
+     * Get remVolume
+     *
+     * @return integer
+     */
+    public function getRemVolume()
+    {
+        return $this->remVolume;
+    }
+
+    /**
+     * Set remIllimited
+     *
+     * @param boolean $remIllimited
+     * @return Offer
+     */
+    public function setRemIllimited($remIllimited)
+    {
+        $this->remIllimited = $remIllimited;
+
+        return $this;
+    }
+
+    /**
+     * Get remIllimited
+     *
+     * @return boolean
+     */
+    public function getRemIllimited()
+    {
+        return $this->remIllimited;
+    }
+
+    /**
+     * Set remMember
+     *
+     * @param string $remMember
+     * @return Offer
+     */
+    public function setRemMember($remMember)
+    {
+        $this->remMember = $remMember;
+
+        return $this;
+    }
+
+    /**
+     * Get remMember
+     *
+     * @return string
+     */
+    public function getRemMember()
+    {
+        return $this->remMember;
+    }
+
+    /**
+     * Set subsidiary
+     *
+     * @param \Web\WebBundle\Entity\Synchro\Subsidiary $subsidiary
+     * @return Offer
+     */
+    public function setSubsidiary(\Web\WebBundle\Entity\Synchro\Subsidiary $subsidiary = null)
+    {
+        $this->subsidiary = $subsidiary;
+
+        return $this;
+    }
+
+    /**
+     * Get subsidiary
+     *
+     * @return \Web\WebBundle\Entity\Synchro\Subsidiary
+     */
+    public function getSubsidiary()
+    {
+        return $this->subsidiary;
     }
 }
