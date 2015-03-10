b0b1a187fb8da91f1398dee1311bbd3c7f4cee2c
Philippe <laurent@natexo.com>
2015-03-10 10:26:47 +0100

    Modif

--- etc/sql/RUBIZZ/Movement.sql
+++ etc/sql/RUBIZZ/Movement.sql
@@ -4,14 +4,14 @@
 CREATE TABLE IF NOT EXISTS RUBIZZ.movement (
     id                  int(10) unsigned NOT NULL COMMENT 'movement id',
     user_id             int(10) unsigned NOT NULL COMMENT 'User id',
-    paymentRequest_id   int(10) unsigned          COMMENT 'Payment request id',
+    payment_request_id  int(10) unsigned          COMMENT 'Payment request id',
     commission_id       int(10) unsigned          COMMENT 'Commission id',
     movement_name       varchar(20)      NOT NULL COMMENT 'Movement Name',
     date_movement       datetime         NOT NULL COMMENT 'Movement Date',
     amount_movement     decimal(10,2)    NOT NULL COMMENT 'Amount mouvement',
     PRIMARY KEY (id),
     CONSTRAINT movementUser_fk FOREIGN KEY (user_id) REFERENCES RUBIZZ.user (id) ON DELETE RESTRICT,
-    CONSTRAINT movementPaymentrequest_fk FOREIGN KEY (paymentRequest_id) REFERENCES RUBIZZ.paymentrequest (id) ON DELETE RESTRICT,
+    CONSTRAINT movementPaymentrequest_fk FOREIGN KEY (payment_request_id) REFERENCES RUBIZZ.payment_request (id) ON DELETE RESTRICT,
     CONSTRAINT movementCommission_fk FOREIGN KEY (commission_id) REFERENCES RUBIZZ.commission (id) ON DELETE RESTRICT,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'Movements';
 
