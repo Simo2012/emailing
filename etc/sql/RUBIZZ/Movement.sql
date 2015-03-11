deea3a3e75c9b49a128993a4dfa939585a308874
GHAZI <GHAZI@192.168.43.100>
2015-03-11 14:24:45 +0100

    By Mohammed : sql mouvement

--- etc/sql/RUBIZZ/Movement.sql
+++ etc/sql/RUBIZZ/Movement.sql
@@ -2,14 +2,15 @@
 -- Table des mouvements
 -- ==============================
 CREATE TABLE IF NOT EXISTS RUBIZZ.movement (
-    id                  int(10) unsigned NOT NULL COMMENT 'movement id',
-    user_id             int(10) unsigned NOT NULL COMMENT 'User id',
-    payment_request_id  int(10) unsigned          COMMENT 'Payment request id',
-    commission_id       int(10) unsigned          COMMENT 'Commission id',
-    movement_name       varchar(20)      NOT NULL COMMENT 'Movement Name',
-    date_movement       datetime         NOT NULL COMMENT 'Movement Date',
-    amount_movement     decimal(10,2)    NOT NULL COMMENT 'Amount mouvement',
+    id                  int(10) unsigned              NOT NULL COMMENT 'movement id',
+    user_id             int(10) unsigned              NOT NULL COMMENT 'User id',
+    payment_request_id  int(10) unsigned                       COMMENT 'Payment request id',
+    commission_id       int(10) unsigned                       COMMENT 'Commission id',
+    movement_type       enum('commission', 'payment') NOT NULL DEFAULT 'Movement type',
+    date_movement       datetime                      NOT NULL COMMENT 'Movement Date',
+    amount_movement     decimal(10,2)                 NOT NULL COMMENT 'Amount mouvement',
     PRIMARY KEY (id),
+    KEY DateUser_idx (date_movement, user_id),
     CONSTRAINT movementUser_fk FOREIGN KEY (user_id) REFERENCES RUBIZZ.user (id) ON DELETE RESTRICT,
     CONSTRAINT movementPaymentrequest_fk FOREIGN KEY (payment_request_id) REFERENCES RUBIZZ.payment_request (id) ON DELETE RESTRICT,
     CONSTRAINT movementCommission_fk FOREIGN KEY (commission_id) REFERENCES RUBIZZ.commission (id) ON DELETE RESTRICT,
