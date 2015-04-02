1fe9d33801028f0c897187aa7199f8b77844e6eb
elias <scicluna@natexo.com>
2015-04-02 10:39:53 +0200

    HIW v2 suppression du js pour l'ancien slider + bug page logué

--- src/Web/WebBundle/Resources/public/js/default/Hiw.js
+++ src/Web/WebBundle/Resources/public/js/default/Hiw.js
@@ -18,79 +18,8 @@ Hiw.prototype = {
      */
     ready: function()
     {
-        $(document).on('click', '.RBZ_hiwMenu', function() {
-            var numberPage = $(this).attr("numSlide");
-            console.log(numberPage);
-            Hiw.prototype._manageChangePage($(this), numberPage);
-            return false;
-        });
-
-        // ==== Clique sur le bouton slide haut ====
-        $(document).on('click', '#span_RBZ_hiwSliderTop', function() {
-            // ---- Trouver la li active ----
-            var liNumberPage = $('li[active]').attr('active');
-            // ---- Récupérer la li précédente ----
-            var loPreviousLi = $('li[active]').prev();
-            if (liNumberPage > 1 && liNumberPage != undefined) {
-                var liPage = liNumberPage - 1;
-                Hiw.prototype._manageChangePage(loPreviousLi, liPage);
-            }
-            return false;
-        });
-        // ==== Clique sur le bouton slide bas ====
-        $(document).on('click', '#span_RBZ_hiwSliderDown', function() {
-            // ---- Trouver la li active ----
-            var liNumberPage = $('li[active]').attr('active');
-            // ---- Récupérer la li précédente ----
-            var loPreviousLi = $('li[active]').next();
-            if (liNumberPage == undefined) {
-                liNumberPage = 1;
-                loPreviousLi = $('.RBZ_hiwMenu').first();
-            }
-            if (liNumberPage < 6 && liNumberPage != undefined) {
-                var liPage = parseInt(liNumberPage) + 1;
-                Hiw.prototype._manageChangePage(loPreviousLi, liPage);
-            }
-            return false;
-        });
 
     }, // ready
-
-    /**
-     * Gestion du changement de page (couleurs , effets etc)
-     */
-    _manageChangePage: function(poElement, piNumberPage)
-    {
-        // ---- Cacher toutes les pages ----
-        $(".RBZ_hiw_page").hide();
-        // ---- Affiche la bonne page ----
-        if (piNumberPage == '1' || piNumberPage == '3' || piNumberPage == '6') {
-            $("body").css("background-color", "#ADE1EC");
-            $("#div_RBZ_hiw_slider").css("color", "#fff");
-            $("#div_RBZ_hiw_slider h3").css("color", "#fff");
-            $(".RBZ_hiwCircle").css("background-color", "#fff");
-        } else {
-            $("body").css("background-color", "#F1F1F1");
-            // ---- menu du slider ----
-            $("#div_RBZ_hiw_slider").css("color", "gray");
-            $("#div_RBZ_hiw_slider h3").css("color", "gray");
-            $(".RBZ_hiwCircle").css("background-color", "gray");
-        }
-        $("#div_RBZ_hiw_p" + piNumberPage).show();
-
-        //---- Supprimer l'effet active ----
-        $(".RBZ_hiwMenu").removeAttr('id', 'li_RBZ_hiwSlideActive');
-        //---- ajouter effet active à l'élément ----
-        $(poElement).attr('id', 'li_RBZ_hiwSlideActive');
-        // ==== Réinitialisation effet active ====
-        $('.RBZ_hiwMenu').css('opacity', '0.9');
-        // ---- Effet active ----
-        $(poElement).css('opacity', '1');
-        // ==== Réinitialisation attribut active ====
-        $('.RBZ_hiwMenu').removeAttr('active');
-        $(poElement).attr('active', piNumberPage);
-
-    }, // _manageChangePage
 
     /**
      * Token de fin
